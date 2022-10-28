9992af261e51c5eecc49bb8ffcd90c7882d899fd
julia-ms <mombachdasilva@gmail.com>
2022-10-28 00:46:29 -0300

    Create index.jsx

---
+++ components/CustomCursor/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+function CustomCursor() {
+
+    return (
+      <div className={styles.btn}>
+        <a href="#" className={styles.btn1}>PASSE O MOUSE AQUI</a>
+        <a href="#" className={styles.btn2}>AQUI TAMBEM</a>
+      </div>
+    );
+  }
+  
+  export default CustomCursor;
